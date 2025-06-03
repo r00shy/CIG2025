05b640630ef3aa8a40125c6c95adcaa4e30140da
Edgar Lange <edgar.lange@st.ovgu.de>
2025-06-03 18:04:45 +0200

    viel wind für nichts , winrate bleibt nahezu gleich :´(

--- doubleLEddy/doubleLBattlePolicy.py
+++ doubleLEddy/doubleLBattlePolicy.py
@@ -8,6 +8,8 @@ from numpy.random import choice
 from vgc2.agent import BattlePolicy
 from vgc2.battle_engine import State, BattleCommand, calculate_damage, BattleRuleParam, BattlingTeam, BattlingPokemon, \
     BattlingMove, TeamView
+from vgc2.battle_engine.damage_calculator import calculate_boosted_stats
+from vgc2.battle_engine.modifiers import Stat
 from vgc2.util.forward import copy_state, forward
 from vgc2.util.rng import ZERO_RNG, ONE_RNG
 from typing import Optional
@@ -76,39 +78,65 @@ class DoubleLBattlePolicy(BattlePolicy):
             alt_target_indices.append(alternativ_target_index)
             alt_max_damages.append(alternativ_max_damage)
 
-        # If both sides have only one Pokémon active, just add one command
-        if len(my_team.active) == 1 and len(opponent_team.active) == 1:
-            commands.append((best_move_indices[0], best_target_indices[0]))
-            return commands
-
         # If you have only one Pokémon, just add one command
         if len(my_team.active) == 1:
             commands.append((best_move_indices[0], best_target_indices[0]))
             return commands
 
-        # If opponent only has one Pokémon, both attack the same target
-        if len(opponent_team.active) == 1:
-            commands.append((best_move_indices[0], best_target_indices[0]))
-            commands.append((best_move_indices[1], best_target_indices[0]))
-            return commands
-
-        # Now apply the lethal check and select final commands
-        # Check if Pokémon 0's best move is lethal
-        if (best_target_indices[0] is not None and
-            max_damages[0] >= opponent_team.active[best_target_indices[0]].hp):
-            # If lethal, second Pokémon tries alternative move if it targets a different Pokémon
-            if (alt_target_indices[1] is not None and
-                alt_target_indices[1] != best_target_indices[0]):
-                commands.append((best_move_indices[0], best_target_indices[0]))
-                commands.append((alt_move_indices[1], alt_target_indices[1]))
-            else:
-                # fallback to best move
-                commands.append((best_move_indices[0], best_target_indices[0]))
-                commands.append((best_move_indices[1], best_target_indices[1]))
-        else:
-            # No lethal move by first Pokémon, both use best moves
+        # If opponent only has one Pokémon and your pokemon have different targets, both use the best move
+        if len(opponent_team.active) == 1 or best_target_indices[0] != best_move_indices[1]:
             commands.append((best_move_indices[0], best_target_indices[0]))
             commands.append((best_move_indices[1], best_target_indices[1]))
+            return commands
 
+        # If the best moves target the same pokemon, lets apply some logic based on our pokemon speed:
+        my_pokemon_0_speed = BattleRuleParam().BOOST_MULTIPLIER_LOOKUP[my_team.active[0].boosts[Stat.SPEED]]* my_team.active[0].constants.stats[Stat.SPEED]
+        my_pokemon_1_speed = BattleRuleParam().BOOST_MULTIPLIER_LOOKUP[my_team.active[1].boosts[Stat.SPEED]]* my_team.active[1].constants.stats[Stat.SPEED]
+
+        fasterIndex = 0
+        slowerIndex = 1
+
+        if my_pokemon_0_speed < my_pokemon_1_speed:
+            fasterIndex = 1
+            slowerIndex = 0
+        
+        # Now apply the lethal check and select final commands
+        # Check if faster Pokémon has a lethal move
+        if (max_damages[fasterIndex] >= opponent_team.active[best_target_indices[fasterIndex]].hp):
+            if (fasterIndex == 0):
+                commands.append((best_move_indices[fasterIndex], best_target_indices[fasterIndex]))
+                commands.append((alt_move_indices[slowerIndex], alt_target_indices[slowerIndex]))
+            else:
+                commands.append((best_move_indices[slowerIndex], best_target_indices[slowerIndex]))
+                commands.append((alt_move_indices[fasterIndex], alt_target_indices[fasterIndex]))
+            return commands
+        
+        if (max_damages[slowerIndex] >= opponent_team.active[best_target_indices[slowerIndex]].hp):
+                if (slowerIndex == 0):
+                    commands.append((best_move_indices[slowerIndex], best_target_indices[slowerIndex]))
+                    commands.append((alt_move_indices[fasterIndex], alt_target_indices[fasterIndex]))
+                else:
+                    commands.append((best_move_indices[fasterIndex], best_target_indices[fasterIndex]))
+                    commands.append((alt_move_indices[slowerIndex], alt_target_indices[slowerIndex]))
+                return commands
+        
+        # Check then if slower Pokémon has a lethal move
+        if (max_damages[fasterIndex] >= opponent_team.active[best_target_indices[fasterIndex]].hp):
+                if (slowerIndex == 0):
+                    commands.append((best_move_indices[slowerIndex], best_target_indices[slowerIndex]))
+                    commands.append((alt_move_indices[fasterIndex], alt_target_indices[fasterIndex]))
+                else:
+                    commands.append((best_move_indices[fasterIndex], best_target_indices[fasterIndex]))
+                    commands.append((alt_move_indices[slowerIndex], alt_target_indices[slowerIndex]))
+                return commands
+                    
+
+        # Otherwise: both use their best moves
+        if (slowerIndex == 0):
+            commands.append((best_move_indices[slowerIndex], best_target_indices[slowerIndex]))
+            commands.append((best_move_indices[fasterIndex], best_target_indices[fasterIndex]))
+        else:
+            commands.append((best_move_indices[fasterIndex], best_target_indices[fasterIndex]))
+            commands.append((best_move_indices[slowerIndex], best_target_indices[slowerIndex]))
         return commands
 
